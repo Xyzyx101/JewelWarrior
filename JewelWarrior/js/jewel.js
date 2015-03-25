5a046e1e9107448e467093878e107a54f88c4248
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-03-25 07:59:00 -0400

    Basic module loader and DOM helper added

---
+++ JewelWarrior/js/jewel.js
@@ -0,0 +1,69 @@
+var jewel = (function () {
+    "use strict";
+    var scriptQueue = []
+            , numResources = 0
+            , numResourcesLoaded = 0
+            , executeRunning = false;
+
+    function load(src, callback) {
+        var queueEntry = {
+            src: src,
+            callback: callback,
+            loaded: false
+        };
+        ++numResources;
+        scriptQueue.push(queueEntry);
+        var image = new Image();
+        image.onload = image.onerror = function () {
+            ++numResourcesLoaded;
+            queueEntry.loaded = true;
+            if (!executeRunning) {
+                executeScriptQueue();
+            }
+        };
+        image.src = src;
+    }
+
+    function executeScriptQueue() {
+        var next = scriptQueue[0]
+            , first
+            , script;
+        if (next && next.loaded) {
+            executeRunning = true;
+            scriptQueue.shift();
+            first = document.getElementsByTagName("script")[0];
+            script = document.createElement("script");
+            script.onload = function () {
+                if (next.callback) {
+                    next.callback();
+                }
+                executeScriptQueue();
+            };
+            script.src = next.src;
+            first.parentNode.insertBefore(script, first);
+        } else {
+            executeRunning = false;
+        }
+    }
+
+    function setup() {
+        console.log("Success!!");
+    }
+
+    function showScreen(screenId) {
+        var dom = jewel.dom
+            , $ = dom.$
+            , activeScreen = $("#game .screen.active")[0]
+            , screen = $("#" + screenId)[0];
+        if (activeScreen) {
+            dom.removeClass(activeScreen, "active");
+        }
+        dom.addClass(screen, "active");
+    }
+
+    return {
+        load: load
+        , setup: setup
+        , showScreen : showScreen
+    };
+})();
